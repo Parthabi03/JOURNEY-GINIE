fee4989dbf955ab82a52ea383ad5bdd2acfbc4f1
Parthabi03 <parthabimaharana90@gmail.com>
2025-02-22 12:17:52 +0530

    all changes

--- src/main/java/edu/rims/Journey_Ginie/controller/LoginController.java
+++ src/main/java/edu/rims/Journey_Ginie/controller/LoginController.java
@@ -1,5 +1,12 @@
 package edu.rims.Journey_Ginie.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class LoginController {
-    
+    @GetMapping("/login")
+    String fragmentLogin() {
+        return "login";
+    }
 }
--- src/main/java/edu/rims/Journey_Ginie/controller/RegistrationController.java
+++ src/main/java/edu/rims/Journey_Ginie/controller/RegistrationController.java
@@ -1,11 +1,22 @@
 package edu.rims.Journey_Ginie.controller;
+
+// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.ModelAttribute;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
+
+// import edu.rims.Journey_Ginie.entity.User;
+// import edu.rims.Journey_Ginie.repository.RegistrationplpRepository;
 
 @Controller
 public class RegistrationController {
-    @GetMapping("/registrationplp")
-    String fragmentRegistrationplp() {
+     @GetMapping("/registrationplp")
+     String fragmentRegistrationplp() {
         return "registrationplp";
     }
 }
+
+
